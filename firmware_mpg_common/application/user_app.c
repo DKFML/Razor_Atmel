d835c7b5fff24e2603a73934f191855706d3e253
DKFML <371113867@qq.com>
2016-06-02 17:55:36 +0800

    buzzer

--- firmware_mpg_common/application/user_app.c
+++ firmware_mpg_common/application/user_app.c
@@ -52,9 +52,6 @@ extern volatile u32 G_u32ApplicationFlags;             /* From main.c */
 extern volatile u32 G_u32SystemTime1ms;                /* From board-specific source file */
 extern volatile u32 G_u32SystemTime1s;                 /* From board-specific source file */
 
-extern u8 G_au8DebugScanfBuffer[];                     /* From debug.c */
-extern u8 G_u8DebugScanfCharCount;                     /* From debug.c  */
-
 
 /***********************************************************************************************************************
 Global variable definitions with scope limited to this local application.
@@ -63,7 +60,7 @@ Variable names shall start with "UserApp_" and be declared as static.
 static fnCode_type UserApp_StateMachine;            /* The state machine function pointer */
 static u32 UserApp_u32Timeout;                      /* Timeout counter used across states */
 
-static u8 au8UserInputBuffer[USER_INPUT_BUFFER_SIZE];  /* Char buffer */
+
 /**********************************************************************************************************************
 Function Definitions
 **********************************************************************************************************************/
@@ -91,27 +88,8 @@ Promises:
 */
 void UserAppInitialize(void)
 {
-  u8 u8String[] = "A string to print that returns cursor to start of next line.\n\r";
-  u8 u8String2[] = "Here's a number: ";
-  u8 u8String3[] = " The 'cursor' was here.";
-  u8 u8String4[] = ".Ye Chenxiao";
-  u32 u32Number = 1234567;
+  PWMAudioSetFrequency(BUZZER1, 500);
   
-  static u8 u8timescounter = 0;
-
-  while(u8timescounter <10)
-  {
-    u8timescounter++;
-    DebugPrintNumber(u8timescounter);
-    DebugPrintf(u8String4);
-    DebugLineFeed();
-    
-  }
-  for(u8 i = 0; i < USER_INPUT_BUFFER_SIZE; i++)
-  {
-    au8UserInputBuffer[i] = 0;
-  }
-
   /* If good initialization, set state to Idle */
   if( 1 )
   {
@@ -142,7 +120,6 @@ Promises:
 */
 void UserAppRunActiveState(void)
 {
-  
   UserApp_StateMachine();
 
 } /* end UserAppRunActiveState */
@@ -161,41 +138,23 @@ State Machine Function Definitions
 /* Wait for a message to be queued */
 static void UserAppSM_Idle(void)
 {
-  static u8 u8NumCharsMessage[] = "\n\rCharacters in buffer:";
-  static u8 u8BufferMessage[]   = "\n\rBuffer contents:\n\r";
-  static u8 u8EmptyMessage[]    = "\n\rBuffer is empty !\n\r";
-  u8 u8CharCount;
-  /* Print message with number of characters in scanf buffer */
-  if(WasButtonPressed(BUTTON0))
-  {
-    ButtonAcknowledge(BUTTON0);
-    
-    DebugPrintf(u8NumCharsMessage);
-    DebugPrintNumber(G_u8DebugScanfCharCount);
-    DebugLineFeed();
-  }
+  static u8 u8button = 0;
+  static u16 u16buzzerrate[]      = {330,294,262,294,330,330,330,294,294,294,330,392,392,330,294,262,294,330,330,330,330,294,294,330,294,262};
+  static u16 u16buzzercounter = 0;
   
-  if(WasButtonPressed(BUTTON1))
+  u16buzzercounter++;
+  
+  if(u16buzzercounter == 800)
   {
-    ButtonAcknowledge(BUTTON1);
+    u16buzzercounter=0;
+    PWMAudioSetFrequency(BUZZER1, u16buzzerrate[u8button]);
+    PWMAudioOn(BUZZER1);
     
-    /* Read the buffer and print the contents */
-    u8CharCount = DebugScanf(au8UserInputBuffer);
-    au8UserInputBuffer[u8CharCount] = '\0';
-    
-    /* Make sure there's at least one character in there! */
-    if(u8CharCount > 0)
+    if(++u8button == 26)
     {
-      DebugPrintf(u8BufferMessage);
-      DebugPrintf(au8UserInputBuffer);
-      DebugLineFeed();
-    }
-    else
-    {
-      DebugPrintf(u8EmptyMessage);
+      u8button=0;
     }
   }
-
 } /* end UserAppSM_Idle() */
      
 
